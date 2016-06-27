1125fd9d576f26f84302ec2317d0e2cc7f183adc
rich <xiegw@xiaolajiao.com>
2016-06-27 16:08:18 +0800

    文件上传

---
+++ upload/script/fileUpload.js
@@ -0,0 +1,26 @@
+;new uploadPreview({ UpBtn: "upload-file", DivShow: "showDiv", ImgShow: "showImg" });
+$(function(){
+    $("#upload-file").on("change",function(event){
+        var file;
+        var destination = document.getElementById('destination');
+        destination.innerHTML = '';
+
+        // 循环用户多选的文件
+        for(var x = 0, xlen = this.files.length; x < xlen; x++) {
+            file = this.files[x];
+            if(file.type.indexOf('image') != -1) { // 非常简单的交验
+
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    var img = new Image();
+                    img.src = e.target.result; // 显示图片的地方
+
+                    destination.appendChild(img);
+                };
+
+                reader.readAsDataURL(file);
+            }
+        }
+    });
+});
